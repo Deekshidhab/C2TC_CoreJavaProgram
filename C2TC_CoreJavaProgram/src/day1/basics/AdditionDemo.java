78782d860f96b5015045489b0304478e8956f253
deekshidhab <deekshidhabakthavachalam@gmail.com>
2025-07-21 16:29:28 +0530

    This is my day1 project

---
+++ C2TC_CoreJavaProgram/src/day1/basics/AdditionDemo.java
@@ -0,0 +1,28 @@
+package day1.basics;
+
+import java.util.Scanner;
+
+public class AdditionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b, c;
+
+		Scanner sc = new Scanner(System.in);
+		
+
+		System.out.println("enter a" );
+        a = sc.nextInt();
+
+		
+
+        System.out.println("enter b" );
+		b = sc.nextInt();
+
+		c = a + b;
+		
+		System.out.println("enter value " + c);
+
+	}
+
+}
--- C2TC_CoreJavaProgram/src/day1/basics/SampleDemo.java
+++ C2TC_CoreJavaProgram/src/day1/basics/SampleDemo.java
@@ -1,10 +1,20 @@
+
 package day1.basics;
 
 public class SampleDemo {
+	static {
+		System.out.println("2");
+	}
+	public static void main(String[] args) 
 
-	public static void main(String[] args) {
+	{
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
 	}
+	static {
+		System.out.println("5");
+	}
 
 }
+
+
